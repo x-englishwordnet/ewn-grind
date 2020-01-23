1fa74ae97be48c5069f309d53d6e56600aa232d7
bbou <bbou@ac-toulouse.fr>
2020-01-23 07:27:49 +0100

    change launch script

--- parse.sh
+++ parse.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-#INDIR=/opt/data/nlp/wordnet/WordNet-2.0/dict
-#INDIR=/opt/data/nlp/wordnet/WordNet-2.1/dict
-#INDIR=/opt/data/nlp/wordnet/WordNet-3.0/dict
-#INDIR=/opt/data/nlp/wordnet/WordNet-3.1/dict
-INDIR=out
+# env variables
+# WNHOME20
+# WNHOME21
+# WNHOME30
+# WNHOME31
+# WNHOMEXX
 
+INDIR="$1"
+if [[ "${INDIR}" =~ ^[[:digit:]] ]]; then
+	case ${INDIR} in
+		20) INDIR="${WNHOME20}" ;;
+		21) INDIR="${WNHOME21}" ;;
+		30) INDIR="${WNHOME30}" ;;
+		31) INDIR="${WNHOME31}" ;;
+		00) INDIR="${WNHOMEXX}" ;;
+	esac
+else
+	if [ -z "${INDIR}" ]; then
+		INDIR=out
+	fi
+fi
+
+echo "${INDIR}"
 java -cp ewn-grind.jar org.ewn.parse.Parser "${INDIR}" 
 
--- parse1.sh
+++ parse1.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
-INDIR20=/opt/data/nlp/wordnet/WordNet-2.0/dict
-INDIR21=/opt/data/nlp/wordnet/WordNet-2.1/dict
-INDIR30=/opt/data/nlp/wordnet/WordNet-3.0/dict
-INDIR31=/opt/data/nlp/wordnet/WordNet-3.1/dict
-INDIRXX=out
+# env variables
+# WNHOME20
+# WNHOME21
+# WNHOME30
+# WNHOME31
+# WNHOMEXX
+
+INDIR="$1"
+if [[ "${INDIR}" =~ ^[[:digit:]] ]]; then
+	case ${INDIR} in
+		20) INDIR="${WNHOME20}" ;;
+		21) INDIR="${WNHOME21}" ;;
+		30) INDIR="${WNHOME30}" ;;
+		31) INDIR="${WNHOME31}" ;;
+		00) INDIR="${WNHOMEXX}" ;;
+	esac
+else
+	if [ -z "${INDIR}" ]; then
+		INDIR=out
+	fi
+fi
 
 # noun|verb|adj|adv
-POS=$1
+POS=$2
+case $POS in
+	n) POS=noun ;;
+	v) POS=verb ;;
+	a) POS=adj ;;
+	s) POS=adj ;;
+	r) POS=adv ;;
+esac
 
 # offset (ie 1740)
-OFS=$2
-
-# version
-VER=$3
-case "$VER" in
-	20) INDIR="${INDIR20}" ;;
-	21) INDIR="${INDIR21}" ;;
-	30) INDIR="${INDIR30}" ;;
-	31) INDIR="${INDIR31}" ;;
-	xx) INDIR="${INDIRXX}" ;;
-esac
+OFS=$3
 
 echo "${INDIR}"
 java -cp ewn-grind.jar org.ewn.parse.LineParser "${INDIR}" ${POS} ${OFS} 
